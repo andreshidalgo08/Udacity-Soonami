edd7ddaafd1ac5dcd483d47e1c6fbb57c16693fd
Andres Hidalgo <andreshidalgo08@gmail.com>
2017-12-25 23:08:49 -0400

    Added some error logging

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -113,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 jsonResponse = makeHttpRequest(url);
             } catch (IOException e) {
-                // TODO Handle the IOException
+                Log.e(LOG_TAG, "IOException", e);
             }
 
             // Extract relevant fields from the JSON response and create an {@link Event} object
@@ -173,8 +173,11 @@ public class MainActivity extends AppCompatActivity {
                     inputStream = urlConnection.getInputStream();
                     jsonResponse = readFromStream(inputStream);
                 }
+                else {
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                }
             } catch (IOException e) {
-                // TODO: Handle the exception
+                Log.e(LOG_TAG, "IOException", e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
